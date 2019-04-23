f0aa0603ba8801fa09b76dcc98b62d675ea503bc
QI LE <1098979923@qq.com>
2019-04-23 22:53:17 +0800

    version -2.0

--- src/com/atguigu/egit/test/TestEgit.java
+++ src/com/atguigu/egit/test/TestEgit.java
@@ -3,6 +3,6 @@ package com.atguigu.egit.test;
 public class TestEgit {
 	public static void main(String[] args) {
 		System.out.println("hello Egit");
-		
+		System.out.println("Goodbye HanZong");
 	}
 }
--- src/com/atguigu/egit/test/TestEgit1.java
+++ src/com/atguigu/egit/test/TestEgit1.java
@@ -3,5 +3,6 @@ package com.atguigu.egit.test;
 public class TestEgit1 {
 	public static void main(String[] args) {
 		System.out.println("hello tomorrow");
+		System.out.println("Goodbye HanZong");
 	}
 }
